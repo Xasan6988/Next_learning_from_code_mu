3294f8cc68a849bc676b7d25dcd75d9e30ad4edc
Atryom Khabibyllin <238136@mail.ru>
2025-12-10 14:58:41 +0300

    dynamic links

--- src/app/prod_with_detail/list/page.tsx
+++ src/app/prod_with_detail/list/page.tsx
@@ -1,7 +1,8 @@
+import Link from 'next/link';
 import prods from '../prods';
 
 export default function List() {
-  const prodsList = prods.map((prod) => <li key={prod.id}>{prod.name}</li>);
+  const prodsList = prods.map((prod) => <li key={prod.id}><Link href={`/prod_with_detail/detail/${prod.id}`}>{prod.name}</Link></li>);
 
   return (
     <ul>{prodsList}</ul>
