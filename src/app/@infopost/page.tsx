632be4be54831146c308cac63dc25b5e7f9e4e28
Atryom Khabibyllin <238136@mail.ru>
2025-12-10 12:57:31 +0300

    parallel routes (?)

---
+++ src/app/@infopost/page.tsx
@@ -0,0 +1,7 @@
+export default function Post() {
+  return (
+        <div>
+            <h1>Hello Post</h1>
+        </div>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
 
 export const metadata: Metadata = {
   title: "Next App from course",
@@ -8,15 +10,25 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  infopost,
+  infouser,
 }: Readonly<{
   children: React.ReactNode;
+  infopost: React.ReactNode,
+  infouser: React.ReactNode,
 }>) {
   return (
     <html lang="en">
       <body>
+        <Header />
         <main>
           {children}
         </main>
+        <section>
+          {infopost}
+          {infouser}
+        </section>
+        <Footer />
       </body>
     </html>
   );
